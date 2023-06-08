17d9b47193e7b13e0d0d6528598f494e59f7d044
Robert Franklin <robertlsfranklin@gmail.com>
2023-06-08 15:24:33 +0800

    update db ops

--- src/db_operations.py
+++ src/db_operations.py
@@ -3,24 +3,19 @@ import pandas as pd
 DB_FILE = "ideas.csv"
 
 def create_table():
+    # Check if ideas.csv exists and if not, create it with column names
     try:
+        df = pd.read_csv('ideas.csv')
+    except FileNotFoundError:
         df = pd.DataFrame(columns=['summary', 'primary_tags', 'secondary_tags', 'link', 'quotes', 'media'])
-        df.to_csv(DB_FILE, index=False)
-    except Exception as e:
-        print(e)
+        df.to_csv('ideas.csv', index=False)
 
 def add_idea(idea):
-    try:
-        df = pd.read_csv(DB_FILE)
-        df.loc[len(df.index)] = list(idea)
-        df.to_csv(DB_FILE, index=False)
-    except Exception as e:
-        print(e)
+    df = pd.read_csv('ideas.csv')
+    new_idea = pd.Series(idea, index=df.columns)
+    df = df.append(new_idea, ignore_index=True)
+    df.to_csv('ideas.csv', index=False)
 
 def fetch_all_ideas():
-    try:
-        df = pd.read_csv(DB_FILE)
-        return df.values.tolist()
-    except Exception as e:
-        print(e)
-        return []
+    df = pd.read_csv('ideas.csv')
+    return df.values.tolist()
