8a92439a1bde5966147b68de7edbd3a3f215ed20
Robert Franklin <robertlsfranklin@gmail.com>
2023-06-08 15:21:04 +0800

    updated to csv database

--- src/db_operations.py
+++ src/db_operations.py
@@ -1,44 +1,26 @@
-import sqlite3
-from sqlite3 import Error
+import pandas as pd
 
-def create_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect('ideas.db', check_same_thread=False)  # Create a persistent database file named ideas.db and allow connections from different threads
-        print(f'successful connection with sqlite version {sqlite3.version}')
-    except Error as e:
-        print(e)
-    return conn
+DB_FILE = "ideas.csv"
 
-def create_table(conn):
+def create_table():
     try:
-        sql_create_ideas_table = """CREATE TABLE IF NOT EXISTS ideas (
-                                        id integer PRIMARY KEY,
-                                        summary text NOT NULL,
-                                        primary_tags text,
-                                        secondary_tags text,
-                                        link text,
-                                        quotes text,
-                                        media text
-                                    ); """
-        conn.execute(sql_create_ideas_table)
-    except Error as e:
+        df = pd.DataFrame(columns=['summary', 'primary_tags', 'secondary_tags', 'link', 'quotes', 'media'])
+        df.to_csv(DB_FILE, index=False)
+    except Exception as e:
         print(e)
 
-def add_idea(conn, idea):
-    sql = ''' INSERT INTO ideas(summary,primary_tags,secondary_tags,link,quotes,media)
-              VALUES(?,?,?,?,?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, idea)
-    conn.commit()
-    return cur.lastrowid
+def add_idea(idea):
+    try:
+        df = pd.read_csv(DB_FILE)
+        df.loc[len(df.index)] = list(idea)
+        df.to_csv(DB_FILE, index=False)
+    except Exception as e:
+        print(e)
 
-def fetch_all_ideas(conn):
-    cur = conn.cursor()
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='ideas';")
-    if cur.fetchone() is not None:
-        cur.execute("SELECT * FROM ideas")
-        rows = cur.fetchall()
-        return rows
-    else:
+def fetch_all_ideas():
+    try:
+        df = pd.read_csv(DB_FILE)
+        return df.values.tolist()
+    except Exception as e:
+        print(e)
         return []
--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 import streamlit as st
-from db_operations import create_connection, create_table, add_idea, fetch_all_ideas
+from db_operations import create_table, add_idea, fetch_all_ideas
 import pandas as pd
 
 def app():
     st.title('Lifetime Thought Tracker')
-    if "conn" not in st.session_state:
-        st.session_state["conn"] = create_connection()
-    create_table(st.session_state["conn"])
+    create_table()
 
     page = st.sidebar.selectbox('Choose a page', ['Summaries', 'Add/Edit Ideas', 'Data'])
 
@@ -15,8 +13,8 @@ def app():
         search_button = st.button('Search')  # Add search button
 
         if search_button:  # Only execute search when button is pressed
-            ideas = fetch_all_ideas(conn)
-            matching_ideas = [idea for idea in ideas if keyword.lower() in idea[1].lower() or keyword.lower() in idea[2].lower()]
+            ideas = fetch_all_ideas()
+            matching_ideas = [idea for idea in ideas if keyword.lower() in idea[0].lower() or keyword.lower() in idea[1].lower()]
 
             if matching_ideas:  # Check if any ideas were found
                 for idea in matching_ideas:
@@ -37,15 +35,15 @@ def app():
 
             if submit_button:
                 idea = (summary, primary_tags, secondary_tags, link, quotes, media)
-                add_idea(conn, idea)
+                add_idea(idea)
                 st.experimental_rerun()
 
     elif page == 'Data':
         st.header("Data View")
-        ideas = fetch_all_ideas(conn)
+        ideas = fetch_all_ideas()
         
         # convert to pandas DataFrame for better visualization
-        ideas_df = pd.DataFrame(ideas, columns=['ID', 'Title', 'Description', 'Primary Tags', 'Secondary Tags', 'Link', 'Quotes'])
+        ideas_df = pd.DataFrame(ideas, columns=['Summary', 'Primary Tags', 'Secondary Tags', 'Link', 'Quotes', 'Media'])
         st.table(ideas_df)
 
 if __name__ == '__main__':
