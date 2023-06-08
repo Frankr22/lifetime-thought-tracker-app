ac4044bbeba6330d92c95fd61ec9a70b17d2ad1d
Robert Franklin <robertlsfranklin@gmail.com>
2023-06-08 15:06:04 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,14 +6,21 @@ def app():
     conn = create_connection()
     create_table(conn)
 
-    page = st.sidebar.selectbox('Choose a page', ['Summaries', 'Add/Edit Ideas'])
+    page = st.sidebar.selectbox('Choose a page', ['Summaries', 'Add/Edit Ideas', 'Data'])
 
     if page == 'Summaries':
         keyword = st.text_input('Enter keyword to search ideas')
-        ideas = fetch_all_ideas(conn)
-        for idea in ideas:
-            if keyword.lower() in idea[1].lower() or keyword.lower() in idea[2].lower():
-                st.write(idea)
+        search_button = st.button('Search')  # Add search button
+
+        if search_button:  # Only execute search when button is pressed
+            ideas = fetch_all_ideas(conn)
+            matching_ideas = [idea for idea in ideas if keyword.lower() in idea[1].lower() or keyword.lower() in idea[2].lower()]
+
+            if matching_ideas:  # Check if any ideas were found
+                for idea in matching_ideas:
+                    st.write(idea)
+            else:  # If no ideas found, display a message
+                st.write('No ideas found for this keyword')
 
     elif page == 'Add/Edit Ideas':
         with st.form(key='ideas_form'):
@@ -30,6 +37,14 @@ def app():
                 idea = (summary, primary_tags, secondary_tags, link, quotes, media)
                 add_idea(conn, idea)
                 st.experimental_rerun()
+
+    elif page == 'Data':
+        st.header("Data View")
+        ideas = fetch_all_ideas(conn)
+        
+        # convert to pandas DataFrame for better visualization
+        ideas_df = pd.DataFrame(ideas, columns=['ID', 'Title', 'Description', 'Primary Tags', 'Secondary Tags', 'Link', 'Quotes'])
+        st.table(ideas_df)
 
 if __name__ == '__main__':
     app()
