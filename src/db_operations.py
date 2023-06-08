ad331efb3d07975ab90944652ab5afc26a74fa31
Robert Franklin <robertlsfranklin@gmail.com>
2023-06-08 15:47:07 +0800

    added edit / delete functionality

--- src/db_operations.py
+++ src/db_operations.py
@@ -16,6 +16,24 @@ def add_idea(idea):
     df = df.append(new_idea, ignore_index=True)
     df.to_csv('ideas.csv', index=False)
 
+def add_or_update_idea(conn, idea, idea_id=None):
+    if idea_id is None:
+        sql = ''' INSERT INTO ideas(summary,primary_tags,secondary_tags,link,quotes,media)
+                  VALUES(?,?,?,?,?,?) '''
+    else:
+        sql = ''' UPDATE ideas SET summary = ?, primary_tags = ?, secondary_tags = ?, link = ?, quotes = ?, media = ? WHERE id = ?'''
+
+    cur = conn.cursor()
+    cur.execute(sql, (*idea, idea_id) if idea_id is not None else idea)
+    conn.commit()
+    return cur.lastrowid
+
 def fetch_all_ideas():
     df = pd.read_csv('ideas.csv')
     return df.values.tolist()
+
+def delete_idea(conn, idea_id):
+    sql = 'DELETE FROM ideas WHERE id=?'
+    cur = conn.cursor()
+    cur.execute(sql, (idea_id,))
+    conn.commit()
--- src/main.py
+++ src/main.py
@@ -1,19 +1,30 @@
 import streamlit as st
-from db_operations import create_table, add_idea, fetch_all_ideas
+from db_operations import create_table, add_or_update_idea, fetch_all_ideas, delete_idea
 import pandas as pd
 
 def app():
+    conn = create_table()
+
     st.title('Lifetime Thought Tracker')
-    create_table()
 
     page = st.sidebar.selectbox('Choose a page', ['Summaries', 'Add/Edit Ideas', 'Data'])
+
+    idea_to_edit = st.sidebar.text_input('Enter the ID of the idea you want to edit')
+    edit_button = st.sidebar.button('Edit Idea')
+
+    if edit_button and idea_to_edit:
+        ideas = fetch_all_ideas(conn)
+        idea_to_edit = next((idea for idea in ideas if idea[0] == int(idea_to_edit)), None)
+
+        if idea_to_edit is not None:
+            page = 'Add/Edit Ideas'
 
     if page == 'Summaries':
         keyword = st.text_input('Enter keyword to search ideas')
         search_button = st.button('Search')  # Add search button
 
         if search_button:  # Only execute search when button is pressed
-            ideas = fetch_all_ideas()
+            ideas = fetch_all_ideas(conn)
             matching_ideas = [idea for idea in ideas if keyword.lower() in (idea[1] or "").lower() or keyword.lower() in (idea[2] or "").lower() or keyword.lower() in (idea[3] or "").lower()]
 
             if matching_ideas:  # Check if any ideas were found
@@ -24,17 +35,17 @@ def app():
 
     elif page == 'Add/Edit Ideas':
         with st.form(key='ideas_form'):
-            summary = st.text_area('Summary', help='Write a summary of your idea or thought')
-            primary_tags = st.text_input('Primary Tags', help='Enter primary tags')
-            secondary_tags = st.text_input('Secondary Tags', help='Enter secondary tags')
-            link = st.text_input('Reference Link', help='Enter the link of reference')
-            quotes = st.text_area('Quotes', help='Enter important quotes')
-            media = st.text_input('Media', help='Enter media links')
+            idea_id = st.hidden_input('Idea ID', value=str(idea_to_edit[0]) if idea_to_edit is not None else '')
+            summary = st.text_area('Summary', value=idea_to_edit[1] if idea_to_edit is not None else '', help='Write a summary of your idea or thought')
+            primary_tags = st.text_input('Primary Tags', value=idea_to_edit[2] if idea_to_edit is not None else '', help='Enter primary tags')
+            secondary_tags = st.text_input('Secondary Tags', value=idea_to_edit[3] if idea_to_edit is not None else '', help='Enter secondary tags')
+            link = st.text_input('Reference Link', value=idea_to_edit[4] if idea_to_edit is not None else '', help='Enter the link of reference')
+            quotes = st.text_area('Quotes', value=idea_to_edit[5] if idea_to_edit is not None else '', help='Enter important quotes')
+            media = st.text_input('Media', value=idea_to_edit[6] if idea_to_edit is not None else '', help='Enter media links')
 
             submit_button = st.form_submit_button(label='Submit')
 
         if submit_button:
-            # Ensure empty fields are treated as an empty string rather than None
             summary = summary or ""
             primary_tags = primary_tags or ""
             secondary_tags = secondary_tags or ""
@@ -42,21 +53,26 @@ def app():
             quotes = quotes or ""
             media = media or ""
 
-            # Make sure the summary is not empty
             if summary.strip() == '':
                 st.error('Summary cannot be empty')
             else:
                 idea = (summary, primary_tags, secondary_tags, link, quotes, media)
-                add_idea(conn, idea)
+                add_or_update_idea(conn, idea, idea_id if idea_id else None)
                 st.experimental_rerun()
 
     elif page == 'Data':
         st.header("Data View")
-        ideas = fetch_all_ideas()
-        
-        # convert to pandas DataFrame for better visualization
-        ideas_df = pd.DataFrame(ideas, columns=['Summary', 'Primary Tags', 'Secondary Tags', 'Link', 'Quotes', 'Media'])
+        ideas = fetch_all_ideas(conn)
+
+        ideas_df = pd.DataFrame(ideas, columns=['ID', 'Summary', 'Primary Tags', 'Secondary Tags', 'Link', 'Quotes', 'Media'])
         st.table(ideas_df)
+
+        delete_idea_id = st.text_input('Enter the ID of the idea you want to delete')
+        delete_button = st.button('Delete Idea')
+
+        if delete_button and delete_idea_id:
+            delete_idea(conn, delete_idea_id)
+            st.experimental_rerun()
 
 if __name__ == '__main__':
     app()
