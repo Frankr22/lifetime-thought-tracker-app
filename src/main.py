3843177575ab844b0f6468d371bc3720e3ece368
Robert Franklin <robertlsfranklin@gmail.com>
2023-06-16 16:27:06 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,76 +1,113 @@
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
 import streamlit as st
 import pandas as pd
-from sqlalchemy import text
+
 
 def app():
-    st.title('Lifetime Thought Tracker')
-    conn = st.experimental_connection('thought_tracker_db', type='sql')
-    conn.execute(text('''CREATE TABLE IF NOT EXISTS ideas
-                (id SERIAL PRIMARY KEY, summary TEXT, primary_tags TEXT, secondary_tags TEXT, link TEXT, quotes TEXT, media TEXT)'''))
+    st.title("Lifetime Thought Tracker")
 
-    page = st.sidebar.selectbox('Choose a page', ['Summaries', 'Add/Edit Ideas', 'Data'])
+    page = st.sidebar.selectbox("Choose a page", ["Homepage", "Data", "Add/Edit Idea"])
 
-    if page == 'Summaries':
-        keyword = st.text_input('Enter keyword to search ideas')
-        search_button = st.button('Search')
+    if page == "Homepage":
+        st.header("Welcome to Lifetime Thought Tracker!")
+        search = st.text_input("Search ideas")  # Add a search box
 
-        if search_button:
-            result = conn.execute(text('SELECT * FROM ideas'))
-            ideas = result.fetchall()
-            matching_ideas = [idea for idea in ideas if keyword.lower() in (idea[1] or "").lower() or keyword.lower() in (idea[2] or "").lower() or keyword.lower() in (idea[3] or "").lower()]
+        # Only run this block if there's input in the search box
+        if search:
+            result = session.execute("SELECT * FROM ideas")
+            df = pd.DataFrame(result.fetchall(), columns=result.keys())
 
-            if matching_ideas:
-                for idea in matching_ideas:
-                    st.write(idea)
-            else:
-                st.write('No ideas found for this keyword')
+            # Make sure any NaN values are replaced with an empty string
+            df['primary_tags'].fillna("", inplace=True)
+            df['secondary_tags'].fillna("", inplace=True)
+            df['summary'].fillna("", inplace=True)
 
-    elif page == 'Add/Edit Ideas':
-        idea_to_edit = None
-        if 'edit_idea_id' in st.session_state:
-            result = conn.execute(text('SELECT * FROM ideas WHERE id = :id'), {'id': st.session_state['edit_idea_id']})
-            idea_to_edit = result.fetchone()
+            # Filter the DataFrame based on the search keyword
+            df = df[df['summary'].str.contains(search, case=False) |
+                    df['primary_tags'].str.contains(search, case=False) |
+                    df['secondary_tags'].str.contains(search, case=False)]
 
-        with st.form(key='ideas_form'):
-            summary = st.text_area('Summary', value=idea_to_edit[1] if idea_to_edit else '', help='Write a summary of your idea or thought')
-            primary_tags = st.text_input('Primary Tags', value=idea_to_edit[2] if idea_to_edit else '', help='Enter primary tags')
-            secondary_tags = st.text_input('Secondary Tags', value=idea_to_edit[3] if idea_to_edit else '', help='Enter secondary tags')
-            link = st.text_input('Reference Link', value=idea_to_edit[4] if idea_to_edit else '', help='Enter the link of reference')
-            quotes = st.text_area('Quotes', value=idea_to_edit[5] if idea_to_edit else '', help='Enter important quotes')
-            media = st.text_input('Media', value=idea_to_edit[6] if idea_to_edit else '', help='Enter media links')
+            # Loop over each idea and create an expander for its details
+            for index, row in df.iterrows():
+                expander = st.expander(row['summary'])
+                with expander:
+                    st.write(f"Primary Tags: {row['primary_tags']}")
+                    st.write(f"Secondary Tags: {row['secondary_tags']}")
+                    st.write(f"Link: {row['link']}")
+                    st.write(f"Quotes: {row['quotes']}")
+                    st.write(f"Media: {row['media']}")
+        else:
+            st.write("Use the search box above to find ideas or use the sidebar to navigate to different pages of the application.")
 
-            submit_button = st.form_submit_button(label='Submit')
+    if page == "Data":
+        keyword = st.text_input("Enter a keyword")
 
-        if submit_button:
-            summary = summary or ""
-            primary_tags = primary_tags or ""
-            secondary_tags = secondary_tags or ""
-            link = link or ""
-            quotes = quotes or ""
-            media = media or ""
+        # Execute the SQL command and fetch all the results
+        result = session.execute("SELECT * FROM ideas")
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
 
-            if summary.strip() == '':
-                st.error('Summary cannot be empty')
-            else:
-                idea = (summary, primary_tags, secondary_tags, link, quotes, media)
-                if 'edit_idea_id' in st.session_state:
-                    conn.execute(text('UPDATE ideas SET summary = :summary, primary_tags = :primary_tags, secondary_tags = :secondary_tags, link = :link, quotes = :quotes, media = :media WHERE id = :id'), {'summary': summary, 'primary_tags': primary_tags, 'secondary_tags': secondary_tags, 'link': link, 'quotes': quotes, 'media': media, 'id': st.session_state['edit_idea_id']})
-                    del st.session_state['edit_idea_id']
-                else:
-                    conn.execute(text('INSERT INTO ideas(summary, primary_tags, secondary_tags, link, quotes, media) VALUES (:summary, :primary_tags, :secondary_tags, :link, :quotes, :media)'), {'summary': summary, 'primary_tags': primary_tags, 'secondary_tags': secondary_tags, 'link': link, 'quotes': quotes, 'media': media})
+        # Make sure any NaN values are replaced with an empty string
+        df['primary_tags'].fillna("", inplace=True)
+        df['secondary_tags'].fillna("", inplace=True)
 
-    elif page == 'Data':
-        result = conn.execute(text('SELECT * FROM ideas'))
-        ideas = result.fetchall()
-        st.write(pd.DataFrame(ideas, columns=['ID', 'Summary', 'Primary Tags', 'Secondary Tags', 'Link', 'Quotes', 'Media']))
-        delete_button = st.button('Delete selected')
+        # Filter the DataFrame based on the keyword
+        if keyword:
+            df = df[
+                (df['summary'].str.lower().str.contains(keyword.lower())) |
+                (df['primary_tags'].str.lower().str.contains(keyword.lower())) |
+                (df['secondary_tags'].str.lower().str.contains(keyword.lower()))
+            ]
 
-        if delete_button:
-            selected_idea = st.selectbox('Select an idea to delete', ideas)
-            if selected_idea:
-                conn.execute(text('DELETE FROM ideas WHERE id = :id'), {'id': selected_idea[0]})
-            else:
-                st.write('No ideas found')
+        # Display the ideas in a table
+        st.dataframe(df)
 
-if __name__ == '__main__':
+        # Add a delete button for each idea
+        for index, row in df.iterrows():
+            if st.button(f"Delete Idea {row['id']}"):
+                # Execute the SQL command to delete the idea
+                session.execute("DELETE FROM ideas WHERE id=:id", {"id": row['id']})
+                session.commit()
+                st.success("Idea deleted successfully!")
+
+    elif page == "Add/Edit Idea":
+        idea_input_fields(session)
+
+def idea_input_fields(session):
+    summary = st.text_input('Summary')
+    primary_tags = st.text_input('Primary Tags')
+    secondary_tags = st.text_input('Secondary Tags')
+    link = st.text_input('Link')
+    quotes = st.text_input('Quotes')
+    media = st.text_input('Media')
+
+    if st.button('Submit'):
+        idea = {
+            'summary': summary, 
+            'primary_tags': primary_tags, 
+            'secondary_tags': secondary_tags, 
+            'link': link, 
+            'quotes': quotes, 
+            'media': media
+        }
+
+        session.execute(
+            text(
+                "INSERT INTO ideas(summary, primary_tags, secondary_tags, link, quotes, media) "
+                "VALUES (:summary, :primary_tags, :secondary_tags, :link, :quotes, :media)"
+            ),
+            idea
+        )
+        session.commit()
+
+
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/thought_tracker_db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+metadata = MetaData()
+ideas = Table('ideas', metadata, autoload_with=engine)
+
+if __name__ == "__main__":
     app()
